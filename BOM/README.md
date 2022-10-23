9d6a14493b6c05abeb7a4d972f1f525737b11e67
dumac13 <dumac13@live.com>
2022-10-23 04:55:27 -0500

    Com

--- BOM/README.md
+++ BOM/README.md
@@ -3,20 +3,14 @@ List of materials
 
 Components
 
-	Arduino Pro Mini
-
-	4 digit 7 Segment display common cathode.
-
-	DS3231 RTC module
-
-	Green led
-
-	Yellow led
-
-	Pin female header / 2x 12 pin, 4 pin
-
-	3x pushbutton
-	Wire 24AWG sorted colors
+Arduino Pro Mini
+4 digit 7 Segment display common cathode.
+DS3231 RTC module
+Green led
+Yellow led
+Pin female header / 2x 12 pin, 4 pin
+3x pushbutton
+Wire 24AWG sorted colors
 
 
 Tools
--- Examples/TestDisplay.ino
+++ Examples/TestDisplay.ino
@@ -1,1 +1,86 @@
+int segApin = 2;
+int segBpin = 3;
+int segCpin = 4;
+int segDpin = 5;
+int segEpin = 6;
+int segFpin = 7;
+int segGpin = 8;
 
+int dig1pin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(__FILE__);
+  delay(1000);
+pinMode(segApin, OUTPUT);
+pinMode(segBpin, OUTPUT);
+pinMode(segCpin, OUTPUT);
+pinMode(segDpin, OUTPUT);
+pinMode(segEpin, OUTPUT);
+pinMode(segFpin, OUTPUT);
+pinMode(segGpin, OUTPUT);
+pinMode(dig1pin, OUTPUT);
+
+digitalWrite(segApin, HIGH);
+digitalWrite(segBpin, HIGH);
+digitalWrite(segCpin, HIGH);
+digitalWrite(segDpin, HIGH);
+digitalWrite(segEpin, HIGH);
+digitalWrite(segFpin, HIGH);
+digitalWrite(segGpin, HIGH);
+
+digitalWrite(dig1pin, LOW);
+digitalWrite(dig2pin, LOW);
+digitalWrite(dig3pin, LOW);
+digitalWrite(dig4pin, LOW);
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+
+digitalWrite(dig1pin, HIGH);
+
+digitalWrite(segApin, LOW);
+delay(1000);
+digitalWrite(segApin, HIGH);
+delay(1000);
+digitalWrite(segBpin, LOW);
+delay(1000);
+digitalWrite(segBpin, HIGH);
+delay(1000);
+digitalWrite(segCpin, LOW);
+delay(1000);
+digitalWrite(segCpin, HIGH);
+delay(1000);
+digitalWrite(segDpin, LOW);
+delay(1000);
+digitalWrite(segDpin, HIGH);
+delay(1000);
+digitalWrite(segEpin, LOW);
+delay(1000);
+digitalWrite(segEpin, HIGH);
+delay(1000);
+digitalWrite(segFpin, LOW);
+delay(1000);
+digitalWrite(segFpin, HIGH);
+delay(1000);
+digitalWrite(segGpin, LOW);
+delay(1000);
+digitalWrite(segGpin, HIGH);
+delay(1000);
+
+delay(5);
+/*digitalWrite(segApin, LOW);
+Serial.println("A - LOW");
+digitalWrite(segBpin, LOW);
+digitalWrite(segCpin, LOW);
+digitalWrite(segDpin, LOW);
+digitalWrite(segEpin, LOW);
+digitalWrite(segFpin, LOW);
+digitalWrite(segGpin, LOW);
+
+delay(1000);
+*/
+}
